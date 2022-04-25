1977fa833256e8cbb9a836c7dabbc8049864d63c
unknown <domain835@gmail.com>
2022-04-25 08:31:03 +0600

    Administration of posts completed

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -34,4 +36,24 @@ class Post extends Model
             ]
         ];
     }
+
+    public static function uploadImage(Request $request, $image = null)
+	{
+		if ($request->hasFile('thumbnail')) {
+			if ($image) {
+				Storage::delete($image);
+			}
+			$folder = date('Y-m-d');
+			return $data['thumbnail'] = $request->file('thumbnail')->store("images/{$folder}");
+		}
+		return null;
+	}
+
+	public  function getImage()
+	{
+		if (!$this->thumbnail) {
+			return asset("no-image.png");
+		}
+		return asset("uploads/{$this->thumbnail}");
+	}
 }
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Редактирование категории</h1>
+                    <h1>Редактирование статьи</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -26,12 +26,15 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Категория "{{ $category->title }}"</h3>
+                            <h3 class="card-title">Статья "{{ $post->title }}"</h3>
                         </div>
 
-                        <form role="form"
-                              method="post"
-                              action="{{ route('categories.update', ['category' => $category->id]) }}">
+                        <form
+                            role="form"
+                            method="post"
+                            action="{{ route('posts.update', ['post' => $post->id]) }}"
+                            enctype="multipart/form-data"
+                        >
                             @csrf
                             @method('PUT')
                             <div class="card-body">
@@ -40,8 +43,71 @@
                                     <input type="text" name="title"
                                            class="form-control @error('title') is-invalid @enderror"
                                            id="title"
-                                           value="{{ $category->title }}">
+                                           value="{{ $post->title }}">
                                 </div>
+
+                                <div class="form-group">
+                                    <label for="description">Цитата</label>
+                                    <textarea name="description"
+                                              class="form-control @error('description') is-invalid @enderror"
+                                              id="description"
+                                              rows="3"
+                                    >{{ $post->description }}
+                                    </textarea>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="content">Контент</label>
+                                    <textarea name="content"
+                                              class="form-control @error('content') is-invalid @enderror"
+                                              id="content"
+                                              rows="5">{{ $post->content }}
+                                    </textarea>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="category_id">Категория</label>
+                                    <select class="form-control @error('category_id') is-invalid @enderror"
+                                            id="category_id"
+                                            name="category_id">
+                                        @foreach($categories as $k => $v)
+                                            <option
+                                                value="{{ $k }}"
+                                                @if($k == $post->category_id) selected @endif
+                                            >
+                                                {{ $v }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="tags">Теги</label>
+                                    <select name="tags[]" id="tags" class="select2" multiple="multiple"
+                                            data-placeholder="Выбор тегов" style="width: 100%;">
+                                        @foreach($tags as $k => $v)
+                                            <option
+                                                value="{{ $k }}"
+                                                @if(in_array($k, $post->tags->pluck('id')->all())) selected @endif
+                                            >
+                                                {{ $v }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="thumbnail">Изображение</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" name="thumbnail" class="custom-file-input"
+                                                   id="thumbnail">
+                                            <label class="custom-file-label" for="thumbnail">Choose file</label>
+                                        </div>
+                                    </div>
+                                    <div><img src="{{ $post->getImage() }}" alt="" class="img-thumbnail mt-2" width="200px"></div>
+                                </div>
+
                             </div>
 
                             <div class="card-footer">
